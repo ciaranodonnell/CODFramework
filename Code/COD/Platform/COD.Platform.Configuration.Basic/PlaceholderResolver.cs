d485e71ceec5f602de12390846a46c89bac3318b
Ciaran O'Donnell <wannabedeveloper@live.com>
2019-05-04 22:27:08 -0400

    Forced commit to change branch, no new functionality yet

---
+++ Code/COD/Platform/COD.Platform.Configuration.Basic/PlaceholderResolver.cs
@@ -0,0 +1,27 @@
+﻿using COD.Platform.Configuration.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COD.Platform.Configuration.Basic
+{
+    public class PlaceholderResolver
+    {
+
+        public static bool Strict = false;
+
+
+        public static string ResolveItem(string itemName, IConfiguration config, string fallbackValue = null)
+        {
+
+            var value = config.GetString(itemName);
+            
+//            value = GetNewNameFromPlaceholder(value);
+
+            return config.GetString(value);
+        }
+
+        
+
+    }
+}
---
+++ Code/COD/Platform/COD.Platform.Configuration.UnitTests/Placeholders/PlaceholderTests.cs
@@ -0,0 +1,24 @@
+﻿using COD.Platform.Configuration.Basic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COD.Platform.Configuration.UnitTests.Placeholders
+{
+    [TestClass]
+    public class PlaceholderTests
+    {
+
+        [TestMethod]
+        public void ResolveSinglePlaceHolder_WholeValue()
+        {
+
+            var newName = PlaceholderResolver.ResolveItem("${NewName}", new BasicConfiguration(null));
+            Assert.AreEqual("NewName", newName, "String should be NewName");
+
+        }
+
+
+    }
+}
