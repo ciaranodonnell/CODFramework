4566b60b5c0104e8e36e41de2bf972368845a8f1
Ciaran O'Donnell <wannabedeveloper@live.com>
2019-10-06 15:49:48 -0400

    Updates and Fixes to StringManipulator

--- Code/COD/Platform/COD.Platform.Framework/StringManipulator.cs
+++ Code/COD/Platform/COD.Platform.Framework/StringManipulator.cs
@@ -96,17 +96,63 @@ namespace COD.Platform.Framework
 
         }
 
+        public string Substring(int start, int length)
+        {
+            char[] result = new char[length];
+
+
+            int offsetOfNode = 0;
+            int foundCount = 0;
+            var node = this.sections.First;
+
+            while (node != null)
+            {
+                var section = node.Value;
+
+
+                if (section.Start < start)
+                {
+                    if ((section.Start + section.Length) > start)
+                    {
+                        //this is the section the right section to start
+                        for (int x = foundCount > 0 ? 0 : (start - section.Start); x < section.Length && foundCount < length; x++)
+                        {
+                            result[foundCount++] = section.Base[section.Start + x];
+                        }
+
+                    }
+
+                    if (foundCount >= length)
+                    {
+                        //found it all
+                        break;
+                    }
+
+                    // we need the next section
+                    node = node.Next;
+                    continue;
+                }
+
+
+
+            }
+            return new string(result);
+
+        }
+
+
         public void Remove(string value, string replaceWith = "")
         {
             int foundLetters = 0;
             bool isFinding = false;
-            int globalIndexOf = -1;
-            int sectionsToGoBack = 0;
 
-            var node = this.sections.First;
+            int sectionsToGoBack = 0;
+            var node = sections.First;
             while (node != null)
             {
                 var section = node.Value;
+
+              // if (section.Length == 0) continue;
 
                 var foundAt = section.Start;
                 var sbase = section.Base;
@@ -116,15 +162,25 @@ namespace COD.Platform.Framework
                     foundLetters = 0;
                     foundAt = section.Start;
 
-                    if ((foundAt = sbase.IndexOf(value[0], foundAt, section.Length)) > -1)
+                    try
+                    {
+                        foundAt = sbase.IndexOf(value[0], foundAt, section.Length);
+                    }catch(Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                    if (foundAt > -1)
                     {
                         //found the first character so starting looping through to find the rest
                         isFinding = true;
                         foundLetters++;
+                        
+                        //keep search till a character mismatch or the end of the section
                         while (isFinding
                             && foundLetters < value.Length
                             && (foundAt + foundLetters) < section.Start + section.Length)
                         {
+                            //check for mismatch
                             if (sbase[foundAt + foundLetters] != value[foundLetters])
                             {
                                 //swing and a miss. 
@@ -136,13 +192,64 @@ namespace COD.Platform.Framework
 
                             foundLetters++;
                         }
+                        
+                        //if isFinding then it was end of section or found it all
                         if (isFinding)
                         {
+                            //check if found it all
                             if (foundLetters == value.Length)
                             {
                                 var newStart = (foundAt + foundLetters);
-                                sections.AddAfter(node, new Section(section.Base, newStart, section.Length - newStart));
-                                section.Length = foundAt - section.Start;
+                                if (newStart < section.Length)
+                                {
+                                    sections.AddAfter(node, new Section(section.Base, newStart, section.Length - newStart));
+                                 
+                      
+                                }
+                                
+                                   section.Length = foundAt - section.Start;
+                                
+                                if (section.Length == 0)
+                                {
+                                    if (node.Previous == null)
+                                    {
+                                        //this was the first node
+                                        sections.RemoveFirst();
+                                        
+                                        if (!string.IsNullOrEmpty(replaceWith))
+                                        {
+                                            //insert new stuff to the beginning, the loop will move us past it
+                                            sections.AddFirst(new Section(replaceWith, 0, replaceWith.Length));
+                                        }
+                                        node = sections.First;
+                                    }
+                                    else
+                                    {
+                                        var temp = node.Previous;
+                                        sections.Remove(node);
+                                        node = temp;
+                                        if (!string.IsNullOrEmpty(replaceWith))
+                                        {
+                                            //insert new stuff before where we are so we dont search that text
+                                            sections.AddAfter(node, new Section(replaceWith, 0, replaceWith.Length));
+                                            node = node.Next;
+                                        }
+
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (!string.IsNullOrEmpty(replaceWith))
+                                    {
+                                        //insert new stuff before where we are so we dont search that text
+                                        sections.AddAfter(node, new Section(replaceWith, 0, replaceWith.Length));
+                                        //move us on to the new node so the loop moves us past it
+                                        node = node.Next;
+                                    }
+
+                                }
+                                isFinding = false;
                             }
                             else
                             {
@@ -158,7 +265,7 @@ namespace COD.Platform.Framework
                     int pointerInThisSection = section.Start;
                     while (isFinding
                             && foundLetters < value.Length
-                            && (pointerInThisSection) < section.Length )
+                            && (pointerInThisSection) < section.Length)
                     {
                         if (sbase[section.Start + pointerInThisSection] != value[foundLetters])
                         {
@@ -186,8 +293,19 @@ namespace COD.Platform.Framework
                                 sections.Remove(node.Previous);
                             }
 
+                            if (section.Length < 1 || section.Start >= section.Base.Length)
+                            {
+                                var oldLast = node.Previous;
+                                sections.Remove(node);
+                                node = oldLast;
+                            }
+
                             //trim end of previous node
-                            node.Previous.Value.Length -= charsInFirstSection;
+                            if (charsInFirstSection > 0)
+                            {
+                                node.Previous.Value.Length -= charsInFirstSection;
+                            }
+                            
 
                         }
                         else
@@ -198,7 +316,7 @@ namespace COD.Platform.Framework
                     }
                 }
                 node = node.Next;
-            }
+            };
         }
 
         public override string ToString()
@@ -227,7 +345,113 @@ namespace COD.Platform.Framework
         }
 
 
+        public int IndexOf(string value, int startIndex = 0)
+        {
+            int foundLetters = 0;
+            bool isFinding = false;
 
+            int sectionsToGoBack = 0;
+            int originalFoundLocation = 0;
+            int stringPassedInPreviousSections = 0;
+            var node = this.sections.First;
+
+            while (node != null)
+            {
+                var section = node.Value;
+
+                var foundAt = section.Start;
+                var sbase = section.Base;
+
+                if (!isFinding)
+                {
+                    foundLetters = 0;
+                    foundAt = section.Start + (Math.Max(startIndex- stringPassedInPreviousSections,0));
+                    
+                    if (
+                        (foundAt < sbase.Length) 
+                        &&
+                        (foundAt = sbase.IndexOf(value[0], foundAt, (section.Length - (foundAt - section.Start)))) > -1
+                       )
+                    {
+                        //found the first character so starting looping through to find the rest
+                        isFinding = true;
+
+                        //record where we started finding;
+                        originalFoundLocation = stringPassedInPreviousSections + foundAt - section.Start;
+
+                        foundLetters++;
+                        while (isFinding
+                            && foundLetters < value.Length
+                            && (foundAt + foundLetters) < section.Start + section.Length)
+                        {
+                            if (sbase[foundAt + foundLetters] != value[foundLetters])
+                            {
+                                //swing and a miss. 
+                                isFinding = false;
+                                foundLetters = 0;
+                                foundAt++;
+                                break;
+                            }
+
+                            foundLetters++;
+                        }
+                        if (isFinding)
+                        {
+                            if (foundLetters == value.Length)
+                            {
+                                //finished finding - shortcut return
+                                return originalFoundLocation;
+                            }
+                            else
+                            {
+                                //must be crossing a section boundary
+                                sectionsToGoBack = 1;
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    int pointerInThisSection = section.Start;
+                    while (isFinding
+                            && foundLetters < value.Length
+                            && (pointerInThisSection) < section.Length)
+                    {
+                        if (sbase[section.Start + pointerInThisSection] != value[foundLetters])
+                        {
+                            //swing and a miss. 
+                            isFinding = false;
+                            foundLetters = 0;
+                            foundAt++;
+                            break;
+                        }
+                        pointerInThisSection++;
+                        foundLetters++;
+                    }
+                    if (isFinding)
+                    {
+                        //Completed Finding the text
+                        if (foundLetters == value.Length)
+                        {
+                            return originalFoundLocation;
+
+                        }
+                        else
+                        {
+                            //must be crossing a section boundary
+                            sectionsToGoBack += 1;
+                        }
+                    }
+                }
+
+
+                stringPassedInPreviousSections += node.Value.Length;
+                node = node.Next;
+
+            }
+            return -1;
+        }
 
         class Stats
         {
